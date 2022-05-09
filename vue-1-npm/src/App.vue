888b7922b625332750fcdafbc9e838baf1e5552b
Tulupov Andrey <tulupov.andrey@gmail.com>
2022-05-09 21:34:14 +0300

    Добавлена папка vue-1-npm

---
+++ vue-1-npm/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button @click="addDislike">Dislike</button>
+        </div>
+        <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+        <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название: </strong>{{ post.title }}</div>
+            <div><strong>Описание: </strong>{{ post.body }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+            posts: [
+                {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+            ]
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1
+        },
+        addDislike() {
+            this.dislikes += 1
+        }
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
