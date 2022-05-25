d59f6fecbb38e279d1ea4750bd6e54898d074188
Tulupov Andrey <tulupov.andrey@gmail.com>
2022-05-25 06:53:47 +0300

    Пагинация переводится в отдельную модель

---
+++ vue-2-api/src/components/PageList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div 
+        v-for="pageNumber in totalPages" 
+        :key="pageNumber"
+        class="page"
+        :class="{
+            'current__page': page === pageNumber
+        }"
+        @click="changePage(pageNumber)"
+    >
+    {{ pageNumber }}
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div {
+    display: flex;
+    margin-top: 15px;
+}
+</style>
