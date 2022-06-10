06049ad82d0658b03bf9c756c9c749e0f6ba30f6
tulupov-com <tulupov.andrey@gmail.com>
2022-06-10 09:23:14 +0300

    добавлена кривая загрузка поста по id

--- vue-3-router/src/pages/PostId.vue
+++ vue-3-router/src/pages/PostId.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
-        <h1>Это пост с ID = {{ $route.params.id }}</h1>           
+        {{ this.fetchPost($route.params.id) }}
+        <h1>Это пост с ID = {{ this.post.id }}, который равен параметру из URL {{ $route.params.id }}</h1>
+        <p><b>Заголовок:</b> {{ this.post.title }}</p>
+        <p><b>Содержимое:</b> {{ this.post.body }}</p>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    
+    data() {
+        return {
+            post: {
+                id: '',
+                title: '',
+                body: ''
+            }
+        }
+    },
+    methods: {
+        async fetchPost(id) {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                    params: {
+                        id: id
+                    }
+                });
+                // console.log(response.data);
+                // console.log(response.data[0]['id']);
+                this.post.id = response.data[0]['id'];
+                this.post.title = response.data[0]['title'];
+                this.post.body = response.data[0]['body'];
+            } catch (error) {
+                alert('Ошибка ', error);
+            }
+        }
+    },
+    computed: {
+        getpost() {
+            this.fetchPost($route.params.id)
+        }
+    }
 }
 </script>
 
